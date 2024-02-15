e1d8e4522af7e7eb30e383c3fb76c4aef66b2b95
vrishab <34526029+securitylamb@users.noreply.github.com>
2024-02-15 16:45:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,3 @@
-# import time
-
-
-# from bs4 import BeautifulSoup
-# from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service
-# from webdriver_manager.chrome import ChromeDriverManager
-
-
-# # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-# driver = webdriver.Chrome(ChromeDriverManager().install())
-
-# url = "https://www.neuralnine.com/books/"
-
-# driver.get(url)
-# soup = BeautifulSoup(driver.page_source,'lxml')
-# headings = soup.find_all('h2',{'class':'elementor-heading-title'})
-
-# for heading in headings:
-#     print(heading.getText())
-
-# time.sleep(10)
-
-# driver.quit()
-
-
 import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
